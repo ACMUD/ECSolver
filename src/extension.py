19aa8841beca5bc05f11380581a24e4383e619ae
Sebastian Martinez <Luismartinez24210@gmail.com>
2025-06-04 20:30:13 -0500

    Feat: Registro de funciones personalizadas
    - Asociar un nombre a una función de Python provista por el usuario
    - Asegurar que los nombres de funciones básicas o reservadas no puedan ser sobrescritos

---
+++ src/extension.py
@@ -0,0 +1,55 @@
+# extension.py
+
+# Este módulo permite al usuario registrar funciones personalizadas
+# y luego usarlas desde otras partes del programa.
+import types
+import inspect
+
+class FunctionRegistry:
+    """
+    Clase para registrar y evaluar funciones personalizadas de forma segura.
+
+    Asegura que las funciones registradas:
+    - No usen nombres reservados como 'SUM', 'IF', etc.
+    """
+    def __init__(self):
+        """Inicializa el registro con funciones permitidas"""
+        self._functions = {}
+        self._reserved = {"SUM", "AVG", "IF", "MAX", "MIN"} 
+        
+    def register(self, name: str, func):
+        """
+        Registra una función en el sistema si es segura, no usa el nombre de una función reservada.
+
+        Parámetros:
+            name (str): Nombre de la función.
+            func (function): Objeto de función a registrar.
+
+        Retorna:
+            None
+        """
+        if name.upper() in self._reserved:
+            print(f"Error: El nombre '{name}' está reservado y no se puede usar.")
+            return
+        if not isinstance(func, types.FunctionType):
+            print(f"Error: lo que intentas registrar no es una función.")
+            return
+
+        print(f"Registrando función {name}")
+        source = inspect.getsource(func)
+        self._functions[name] = {"func": func, "source": source}
+        print(f"Función '{name}' registrada con éxito.")
+
+
+    def get_registered_functions(self):
+        """
+        Retorna una lista de funciones registradas, incluyendo nombre y código fuente.
+
+        Retorna:
+            list[dict]: Lista de funciones con claves 'name' y 'source'.
+        """
+        return [
+            {"name": name, "source": data["source"]}
+            for name, data in self._functions.items()
+        ]
+    
